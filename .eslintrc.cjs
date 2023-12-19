96c6af77fe11f9dff10c3d03a02314fbfa3727cd
woajun <woajun@naver.com>
2023-12-19 19:30:19 +0900

    feat: add airbnb eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,43 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript'
   ],
+  parserOptions: {
+    project: "./tsconfig.json"
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs', 'postcss.config.js'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    "import/no-extraneous-dependencies": "off",
+    "object-curly-newline": "off",
+    "react/require-default-props": "off",
+    "no-plusplus": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "jsx-a11y/label-has-associated-control": "off",
+    "linebreak-style": "off",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "react/function-component-definition": ["error", { "namedComponents": "arrow-function" }],
+    "func-style": ["error", "expression"],
+    "import/extensions": "off",
+    "@typescript-eslint/no-unnecessary-type-constraint": ["off"],
+    "no-restricted-imports": ["error", {
+      "patterns": ["..*"]
+    }],
+    "jsx-a11y/no-static-element-interactions": "off",
+    "jsx-a11y/click-events-have-key-events": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/no-unescaped-entities": "off",
+    "react/prop-types": "off",
+    "jsx-a11y/control-has-associated-label": "off",
+    "import/no-named-default": "off",
+    "import/prefer-default-export": "off",
+    "arrow-body-style": "off",
   },
 }
--- src/showcase/ComponentsNavigation.tsx
+++ src/showcase/ComponentsNavigation.tsx
@@ -1,47 +1,47 @@
-import ComponentsNavigationButton from "./ComponentsNavigationButton"
+import ComponentsNavigationButton from './ComponentsNavigationButton';
 
-function ComponentsNavigation () {
-    const data = [
-        {
-            label: 'bottom drawer',
-            to: '/bottom-drawer'
-        },
-        {
-            label: 'dim',
-            to: '/dim'
-        },
-        {
-            label: 'chart',
-            to: '/chart'
-        },
-        {
-            label: 'picker',
-            to: '/picker'
-        },
-        {
-            label: 'modal',
-            to: '/modal'
-        },
-        {
-            label: 'card-bundle',
-            to: '/card-bundle'
-        },
-        {
-            label: 'card-bundle',
-            to: '/card-bundle'
-        },
-        {
-            label: 'tab',
-            to: '/tab'
-        },
-    ]
-    return (
-        <div className="grid grid-cols-3 text-center md:grid-cols-5 gap-2" >
-            {data.map((e) => (
-                <ComponentsNavigationButton data={e}/>
-            ))}
-        </div>
-    )
+function ComponentsNavigation() {
+  const data = [
+    {
+      label: 'bottom drawer',
+      to: '/bottom-drawer',
+    },
+    {
+      label: 'dim',
+      to: '/dim',
+    },
+    {
+      label: 'chart',
+      to: '/chart',
+    },
+    {
+      label: 'picker',
+      to: '/picker',
+    },
+    {
+      label: 'modal',
+      to: '/modal',
+    },
+    {
+      label: 'card-bundle',
+      to: '/card-bundle',
+    },
+    {
+      label: 'card-bundle',
+      to: '/card-bundle',
+    },
+    {
+      label: 'tab',
+      to: '/tab',
+    },
+  ];
+  return (
+    <div className="grid grid-cols-3 text-center md:grid-cols-5 gap-2">
+      {data.map((e) => (
+        <ComponentsNavigationButton data={e} />
+      ))}
+    </div>
+  );
 }
 
-export default ComponentsNavigation
+export default ComponentsNavigation;
